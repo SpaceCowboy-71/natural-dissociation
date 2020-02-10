788561b0d0150cf91b39290287311874f712f385
SpaceCowboy-71 <prof.schoenmann@live.de>
2020-02-10 15:19:55 +0100

    Added carbon Loading component to fallback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { lazy, Suspense } from "react";
+import { Loading } from 'carbon-components-react';
 import "./App.scss";
+import { InlineLoading } from "carbon-components-react";
 
 const MainHeader = lazy(() => import("./components/MainHeader"));
 const Content = lazy(() => import("./components/Content/Content"));
@@ -7,7 +9,7 @@ const Content = lazy(() => import("./components/Content/Content"));
 const App = () => {
   return (
     <>
-      <Suspense fallback="">
+      <Suspense fallback={<Loading/>}>
         <MainHeader />
       </Suspense>
       <Suspense fallback="">
