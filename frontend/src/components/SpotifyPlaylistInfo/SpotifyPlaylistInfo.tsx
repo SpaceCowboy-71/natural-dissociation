5c5551f94d75575d95b6841008b9da713309a289
SpaceCowboy-71 <prof.schoenmann@live.de>
2020-02-11 22:50:27 +0100

    Added ClickableTile prototype to SpotifyPlaylistInfo

--- frontend/src/components/SpotifyPlaylistInfo/SpotifyPlaylistInfo.tsx
+++ frontend/src/components/SpotifyPlaylistInfo/SpotifyPlaylistInfo.tsx
@@ -9,6 +9,7 @@
 //
 import firebase from "firebase";
 import React, { memo, ReactNode } from "react";
+import { ClickableTile } from "carbon-components-react";
 import { SpotifyPlaylistMetadata } from "../../model/data.types";
 import { useParams } from "react-router-dom";
 import {
@@ -45,9 +46,16 @@ export default function<SpotifyPlaylistInfoProps>(props) {
         <div className="bx--row playlist-info-page__banner bx--no-gutter">
           <div className="bx--col-lg-16 bx-.bx--no-gutter--right">
             <img className="playlist-image" src={media} />
+            <div className="playlist-tile-container bx--col-lg-4 bx--offset-lg-8 bx--col-md-3 bx--offset-md-4 bx--col-sm-2 bx--offset-sm-2">
+              <ClickableTile
+                className="playlist-tile-spotify"
+                href={playlist?.spotify.playlistUrl}
+              >
+                Clickable
+              </ClickableTile>
+            </div>
           </div>
         </div>
-
         <div className="bx--row playlist-info-page__body">
           <div className="bx--col-md-4 bx--offset-lg-3 bx--col-lg-5">
             <h2 className="playlist-info-page__heading">{playlist?.title}</h2>
