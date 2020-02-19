814d48daf63018ae6187cb14dd6a76c94d27c200
SpaceCowboy-71 <prof.schoenmann@live.de>
2020-02-19 23:41:30 +0100

    Edit small scss styling

--- frontend/src/model/playlist.ts
+++ frontend/src/model/playlist.ts
@@ -7,6 +7,7 @@ export interface SpotifyPlaylistMetadata {
   tags: SpotifyPlaylistTag[];
 }
 
+//TODO: doesn't work right now (doesn't enforce type)
 export enum SpotifyPlaylistTag {
   Disco = "Disco", 
   Summer ="Summer"
